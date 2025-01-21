f136d088adaf8151287a7f3e48456ccc09d707e6
Abhi <abhishekkumar2505122001@gmail.com>
2025-01-21 15:17:31 +0530

    edit 2

--- Assignments/7Assign_file_seggregation.py
+++ Assignments/7Assign_file_seggregation.py
@@ -1,14 +1,13 @@
 import os
 import shutil
 
-for i in os.listdir(r'E:\\Python\\Bizmetric python\\'):
-    fnm=r'E:\\Python\\Bizmetric python\\'+i
-    #sz=(os.stat('E:\\Python\\Bizmetric python\\'+i).st_size)/1024
+for i in os.listdir(r'E:\\Python\\python_assign\\'):
+    fnm=r'E:\\Python\\python_assign\\'+i
     sz=os.stat(fnm).st_size/1000
 
     if 0<=sz<=200:
-        shutil.move(fnm,'E:\\Python\\Bizmetric python\\smoll')
+        shutil.move(fnm,'E:\\Python\\python_assign\\smoll')
     elif 200<=sz<=400:
-        shutil.move(fnm,'E:\\Python\\Bizmetric python\\mid')
+        shutil.move(fnm,'E:\\Python\\python_assign\\mid')
     else:
-        shutil.move(fnm,'E:\\Python\\Bizmetric python\\lorge')
+        shutil.move(fnm,'E:\\Python\\python_assign\\lorge')
