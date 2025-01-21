27ed7dc9bc82a071d56ce63057664911770c1233
Abhi <abhishekkumar2505122001@gmail.com>
2025-01-21 15:18:54 +0530

    Adding assignments and database files

--- Assignments/7Assign_file_seggregation.py
+++ Assignments/7Assign_file_seggregation.py
@@ -6,8 +6,8 @@ for i in os.listdir(r'E:\\Python\\python_assign\\'):
     sz=os.stat(fnm).st_size/1000
 
     if 0<=sz<=200:
-        shutil.move(fnm,'E:\\Python\\python_assign\\smoll')
+        shutil.move(fnm,'E:\\Python\\python_assign\\small')
     elif 200<=sz<=400:
         shutil.move(fnm,'E:\\Python\\python_assign\\mid')
     else:
-        shutil.move(fnm,'E:\\Python\\python_assign\\lorge')
+        shutil.move(fnm,'E:\\Python\\python_assign\\large')
